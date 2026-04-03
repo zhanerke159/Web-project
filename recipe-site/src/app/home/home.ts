0b40692aaf605b342acaa5fc616d6d1afa61ef6b
zhanerke159 <z_manapaly@kbtu.kz>
2026-04-03 23:59:57 +0500

    feat: почти завершен дизайн главной со слайдером и навигацией

--- recipe-site/src/app/home/home.ts
+++ recipe-site/src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit, OnDestroy } from '@angular/core';
 import { FormsModule } from '@angular/forms'; 
 import { CommonModule } from '@angular/common'; 
 
@@ -10,11 +10,10 @@ import { CommonModule } from '@angular/common';
   styleUrl: './home.css',
   encapsulation: ViewEncapsulation.None
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
   searchQuery: string = '';
-  onSearch() {
-    console.log('Searching for:', this.searchQuery);
-  }
+  currentSlideIndex = 0;
+  private slideTimer: any;
 
   categories = [
     { name: 'Fast Food', icon: '🍔', color: '#FDE68A' },
@@ -24,4 +23,34 @@ export class HomeComponent {
     { name: 'Breakfast', icon: '🍳', color: '#DBEAFE' },
     { name: 'Main Dishes', icon: '🍝', color: '#F3E8FF' }
   ];
+
+  slides = [
+    'https://i.pinimg.com/1200x/86/2b/a7/862ba759d5444daddbf03bd07e94eb4d.jpg',
+    'https://i.pinimg.com/1200x/e3/0e/56/e30e5616b2b6abc44dff773d15539614.jpg',
+    'https://i.pinimg.com/1200x/ba/b3/18/bab31873cf89cde0aae2635159ca508a.jpg'
+  ];
+
+  ngOnInit() {
+    this.startCarousel();
+  }
+
+  startCarousel() {
+    this.slideTimer = setInterval(() => {
+      this.nextSlide();
+    }, 3000);
+  }
+
+  nextSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slides.length;
+  }
+
+  onSearch() {
+    console.log('Searching for:', this.searchQuery);
+  }
+
+  ngOnDestroy() {
+    if (this.slideTimer) {
+      clearInterval(this.slideTimer);
+    }
+  }
 }
