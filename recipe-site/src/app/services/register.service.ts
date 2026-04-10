f52e8f5f49ed4195051682c111700c3dbdbcdcdc
zhanerke159 <z_manapaly@kbtu.kz>
2026-04-10 18:30:57 +0500

    feat: login logic works, tokens received

--- recipe-site/src/app/services/register.service.ts
+++ recipe-site/src/app/services/register.service.ts
@@ -11,4 +11,8 @@ export class AuthService {
   register(userData: any): Observable<any> {
     return this.http.post(this.apiUrl, userData);
   }
+
+  login(credentials: any) {
+  return this.http.post('http://127.0.0.1:8000/api/token/', credentials);
+}
 }
