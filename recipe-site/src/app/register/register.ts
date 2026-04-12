2f16ba233903a72e9163666d2340d96f21b8c78a
zhanerke159 <z_manapaly@kbtu.kz>
2026-04-12 16:19:21 +0500

    just changed

--- recipe-site/src/app/register/register.ts
+++ recipe-site/src/app/register/register.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
