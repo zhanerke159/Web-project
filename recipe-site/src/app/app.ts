44759d061627af6f407a615851081b0016722683
zhanerke159 <z_manapaly@kbtu.kz>
2026-04-12 19:06:50 +0500

    добавила поиск

--- recipe-site/src/app/app.ts
+++ recipe-site/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { RouterOutlet, RouterLink, Router } from '@angular/router';
+import { ApiService } from './services/recipe';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink,  CommonModule, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -15,4 +18,35 @@ export class App {
     const section = document.getElementById(sectionId);
     section?.scrollIntoView({ behavior: 'smooth' });
   }
-}
+
+  searchResults: any[] = [];
+  searchQuery: string = '';
+
+ constructor(
+    private apiService: ApiService, 
+    private router: Router
+  ) {}
+
+  onSearch() {
+    if (!this.searchQuery || this.searchQuery.trim() === '') {
+      this.searchResults = [];
+      return;
+    }
+    const queryObj = { title: this.searchQuery };
+
+    this.apiService.searchRecipes(queryObj).subscribe({
+      next: (data: any) => {
+        this.searchResults = data;
+      },
+      error: (err) => {
+        console.error('Ошибка при поиске:', err);
+      }
+    });
+  }
+
+  goToRecipe(id: number) {
+    this.router.navigate(['/recipe', id]);
+    this.searchQuery = ''; 
+    this.searchResults = [];
+  }
+} 
